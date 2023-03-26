a195e5e1e0714c64c62fddb00ba582eab75da864
younes-benniz <bennizyouness@gmail.com>
2023-03-26 00:39:31 +0000

    fetch cities on user Search

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
 import { useNavigate } from "@tanstack/react-location";
+import AsyncSelect from "react-select/async";
+import axios from "axios";
 
 const SearchBar = () => {
-	const [search, setSearch] = useState("");
 	const navigate = useNavigate();
 
-	const handleSearch = () => {
-		navigate({ to: `/weather/${search}`, replace: true });
-		setSearch("");
+	const loadOptions = async (searchValue, callback) => {
+		const response = await axios.get(
+			`https://geocoding-api.open-meteo.com/v1/search?name=${searchValue}`,
+		);
+		const filtred = response.data.results.map((item) => {
+			item.label = item.name;
+			item.value = item.name.toLowerCase();
+			return item;
+		});
+		callback(filtred);
+	};
+
+	const handleSelect = (itemObject) => {
+		navigate({ to: `/weather/${itemObject.value}`, replace: true });
 	};
 
 	return (
 		<div className="flex gap-5 w-full justify-center">
-			<input
-				type="text"
-				className="input input-bordered input-primary w-full max-w-xs"
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
+			<AsyncSelect
+				className="w-full max-w-xs"
+				placeholder="Search..."
+				cacheOptions
+				loadOptions={loadOptions}
+				onChange={(itemObject) => handleSelect(itemObject)}
 			/>
-			<button className="btn btn-success" onClick={handleSearch}>
-				Search
-			</button>
 		</div>
 	);
 };
