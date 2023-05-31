44778f03302fd23983084c2b9d748b56edaf998a
younes-benniz <bennizyouness@gmail.com>
2023-05-31 12:38:38 +0100

    store theme state in localStorage

--- src/components/ThemeSwap/index.jsx
+++ src/components/ThemeSwap/index.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi";
 
 const ThemeSwap = () => {
-	const [theme, setTheme] = useState("light");
+	const [isDark, setIsDark] = useState(false);
 
-	const handleThemeChange = () => {
-		setTheme((state) => (state === "light" ? "dark" : "light"));
-	};
-
-	console.log("theme", theme);
+	useEffect(() => {
+		if (
+			localStorage.theme === "dark" ||
+			(!("theme" in localStorage) &&
+				window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			document.documentElement.classList.add("dark");
+			setIsDark(true);
+		} else {
+			document.documentElement.classList.remove("dark");
+			setIsDark(false);
+		}
+	}, [isDark]);
 
 	return (
 		<label className="swap swap-rotate">
-			<input type="checkbox" onChange={handleThemeChange} checked={theme === "dark"} />
+			<input
+				type="checkbox"
+				onChange={() => {
+					setIsDark(!isDark);
+					localStorage.theme = !isDark ? "dark" : "light";
+				}}
+				checked={isDark}
+			/>
 			<HiOutlineSun className="swap-off w-10 h-10" />
 			<HiOutlineMoon className="swap-on w-10 h-10" />
 		</label>
