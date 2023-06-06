ac2bcb57419028022b98adc1e886d6efdd4daf05
younes-benniz <bennizyouness@gmail.com>
2023-06-06 17:03:16 +0100

    fix brand href

--- src/components/Brand/index.jsx
+++ src/components/Brand/index.jsx
@@ -1,7 +1,7 @@
 const Brand = () => (
 	<div className="flex lg:flex-1">
 		<h1 className="w-auto font-extrabold text-3xl tracking-wide leading-8">
-			<a href="/" className="-m-1.5 p-1.5">
+			<a href={import.meta.env.BASE_URL} className="-m-1.5 p-1.5">
 				WeatherWise
 			</a>
 		</h1>
